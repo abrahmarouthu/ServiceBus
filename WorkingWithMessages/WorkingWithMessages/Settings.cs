c83f9dec5be780f3fa79021f4927d11977b80f3c
Ashwin Brahmarouthu <bashwin.87@gmail.com>
2020-03-15 15:48:20 -0700

    Messages - Send and Recieve

---
+++ WorkingWithMessages/WorkingWithMessages/Settings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WorkingWithMessages.Config
+{
+    public class Settings
+    {
+        public static string ConnectionString = "Endpoint=sb://ashwintest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9QDdwVLnZ6RyYfcj67WPeFafg+Qlc1rjly1gNNJ7aSA=";
+        public static string QueueName = "workingwithmessages";
+    }
+}
