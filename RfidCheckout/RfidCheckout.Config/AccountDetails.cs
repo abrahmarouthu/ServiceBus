8f87e32a1e74cbb381b419180af17f6222c1e8ce
Ashwin Brahmarouthu <bashwin.87@gmail.com>
2020-03-15 19:08:39 -0700

    Receiving & Processing Messages, Duplicate Detection

---
+++ RfidCheckout/RfidCheckout.Config/AccountDetails.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RfidCheckout.Config
+{
+    public class AccountDetails
+    {
+        //ToDo: Enter a valid Serivce Bus connection string
+        public static string ConnectionString = "Endpoint=sb://ashwintest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9QDdwVLnZ6RyYfcj67WPeFafg+Qlc1rjly1gNNJ7aSA=";
+        public static string QueueName = "rfidcheckout";
+    }
+}
