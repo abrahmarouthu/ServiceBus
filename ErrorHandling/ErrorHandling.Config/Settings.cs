782677b1969d753fccc51493466c4be2585eccba
Ashwin Brahmarouthu <bashwin.87@gmail.com>
2020-03-19 10:28:02 -0700

    Setup config file

---
+++ ErrorHandling/ErrorHandling.Config/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Config
+{
+    public static class Settings
+    {
+        //ToDo: Enter a valid Service Bus connection string
+        public static string ConnectionString = "Endpoint=sb://ashwintest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9QDdwVLnZ6RyYfcj67WPeFafg+Qlc1rjly1gNNJ7aSA=";
+        public static string QueuePath = "errorhandling";
+        public static string ForwardingQueuePath = "errorhandlingforwarding";
+    }
+}
