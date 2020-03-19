5541509ae9c23a13ccbd413e1b9db8fca3c5bbfb
Ashwin Brahmarouthu <bashwin.87@gmail.com>
2020-03-19 09:42:13 -0700

    cleanup

--- RequestResponseMessaging/RequestResponseMessaging.Config/AccountDetails.cs
+++ RequestResponseMessaging/RequestResponseMessaging.Config/AccountDetails.cs
@@ -10,7 +10,6 @@ namespace RequestResponseMessaging.Config
 
         //ToDo: Enter a valid Serivce Bus connection string
         public static string ConnectionString = "Endpoint=sb://ashwintest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9QDdwVLnZ6RyYfcj67WPeFafg+Qlc1rjly1gNNJ7aSA=";
-
         public static string RequestQueueName = "requestqueue";
         public static string ResponseQueueName = "responsequeue";
 
